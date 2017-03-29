902f6a8b1034a596345ff7e71cf358ba213ed1d7
markdegroat <mhdegroat@gmail.com>
2017-03-29 13:21:12 -0400

    Recipe.find_by_ingredient_list is now working when sets of ingredients match exactly

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,16 +6,8 @@ class Recipe < ActiveRecord::Base
   has_many :user_recipes
 
 	def self.find_by_ingredient_list(ingredient_list)
-		Recipe.all.each do |recipe|
-			if recipe.ingredients.sort == ingredient_list.sort
-			#if (recipe.ingredients & ingredient_list) == recipe.ingredients
-
-				recipe.title
-				puts 'THIS IS HAPPENING'
-				break
-			else
-				"NOT FOUND"
-			end
+		self.all.select do |recipe|
+			recipe.ingredients.sort == ingredient_list.sort
 		end
 	end
 

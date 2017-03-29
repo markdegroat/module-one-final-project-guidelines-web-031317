90704b47b41560d07f19bde8c8a00de40c1583e7
markdegroat <mhdegroat@gmail.com>
2017-03-29 11:49:00 -0400

    Started defining and testing methods for the user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,26 @@ class User < ActiveRecord::Base
   has_many :user_recipes
 
   has_many :ingredients, through: :fridges
+  has_many :fridges
+
+  def display_fridge
+    self.ingredients.each_with_index do |ingredient, index|
+      puts "#{index+1}. #{ingredient.name}"
+    end
+  end
+
+  def add_ingredient_to_fridge(ingredient_name)
+    ingredient = Ingredient.find_by_name(ingredient_name)
+    if !!ingredient
+      self.ingredients << ingredient
+      self.save
+    else
+      self.ingredients.build(name: ingredient_name)
+      self.save
+    end
+  end
+
+  def fridge
+    self.ingredients
+  end
 end

c1c61af156dd5181989ae2d9406fa5ecc9d6c4b5
github username <github email address>
2017-03-29 14:40:59 -0400

    testing

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,16 +6,11 @@ class Recipe < ActiveRecord::Base
   has_many :user_recipes
 
 	def self.find_by_ingredient_list(ingredient_list)
-<<<<<<< HEAD
     fridge_ingredients = Set.new(ingredient_list)
 
 		Recipe.all.select do |recipe|
 			recipe_ingredients = Set.new(recipe.ingredients)
       recipe_ingredients.subset?(fridge_ingredients) && !recipe_ingredients.empty?
-=======
-		self.all.select do |recipe|
-			recipe.ingredients.sort == ingredient_list.sort
->>>>>>> 902f6a8b1034a596345ff7e71cf358ba213ed1d7
 		end
 	end
 

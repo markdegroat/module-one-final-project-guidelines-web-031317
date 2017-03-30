8a35e0a31dbea6358b05249616a5419595d1c33a
markdegroat <mhdegroat@gmail.com>
2017-03-30 16:41:10 -0400

    Our api now fills our database recipes and the ingredients needed to make them, still need to add the instructions to each of the recipes

--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,7 @@ require 'json'
 
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+ActiveRecord::Base.logger= nil
 require_all 'lib'
 require_all 'app'
-require_all 'db'
+# require_all 'db'

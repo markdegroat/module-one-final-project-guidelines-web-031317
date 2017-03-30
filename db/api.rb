5128cb7ab57b5b96d3eccc6c8bc020fc6f5e1986
markdegroat <mhdegroat@gmail.com>
2017-03-30 15:01:23 -0400

    fleshed out a way to strip certain words for the ingredients, trying to clean up the data so when we add to our db it won't have extraneous words or characters in the name of the ingredient

--- db/api.rb
+++ db/api.rb
@@ -2,14 +2,43 @@
 require 'json'
 require "rest-client"
 require 'pry'
+require 'pp'
 response = RestClient.get('https://api.edamam.com/search?q=chicken&app_id=81a71c4e&app_key=52085f9209f9d0b19ee7a21fb9f0cdd6')
 data = JSON.parse(response)
+#binding.pry
+puts "EOF"
+class String
+  def strip_ingredient_list
+
+    self.split(/[\s,()]/).reject do |string|
+      string.include?("tablespoon")  || string.include?("teaspoon") || string.include?("cup") || /\A\d+\z/.match(string) ||  string.include?("(") || string.include?(")") || string.include?(",") || string.include?("/") || string.include("-")
+    end.join(' ')
+  end
+end
+
+#data["hits"][0]["ingredients"]
+
+#ingredient list
+#data["hits"][0]["recipe"]["ingredients"]
+#individual ingredients
+#data["hits"][0]["recipe"]["ingredients"].collect{|ingredient| ingredient["text"]}
+#recipe title
+#data["hits"][0]["recipe"]["label"]
+
+
+#filling a sample array to test our regex to extract ingredient names
+sample_array = []
+data["hits"].each do |hit|
+
+  sample_array += hit["recipe"]["ingredients"].collect{|ingredient| ingredient["text"]}
+end
+puts sample_array
 binding.pry
 puts "EOF"
 
-data["hits"][0]["ingredients"]
+single_test = sample_array[1].split(/[\s,()]/)
+single_test = single_test.reject do |string|
+  string.include?("tablespoon")  || /\A\d+\z/.match(string) ||  string.include?("(") || string.include?(")") || string.include?(",")
+end
 
-#ingredient list
-data["hits"][0]["recipe"]["ingredients"]
-#recipe title
-data["hits"][0]["recipe"]["label"]
+puts sample_array.collect{|ingredient_list| ingredient_list.strip_ingredient_list}
